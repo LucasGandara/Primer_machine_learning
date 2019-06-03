58a4e6506c7114bc39f43c1afcc1e4d5fc12b915
LucasGandara <49881497+LucasGandara@users.noreply.github.com>
2019-06-03 17:42:31 -0500

    Update Compuerta Xor.py
    Utilizacion de la libreria con la compuerta XOR

--- Compuerta Xor.py
+++ Compuerta Xor.py
@@ -1,4 +1,37 @@
 import numpy as np #Para los arreglos 
 import sklearn
-from sklearn.datasets import load_iris # arreglos de flores de iris
 from sklearn.model_selection import train_test_split # Para dividir los datos en sets de entradas y sets de entrenamiento 
+from sklearn.neighbors import KNeighborsClassifier
+
+# Para mi compuerta Xor debo crear los arreglos como los espera la clase sklearn
+X_aux = [  [0,0],
+           [0,1],
+           [1,0],
+           [1,1]];
+
+Y_aux = [1,0,0,1]
+
+
+X_train = []
+Y_train = []
+for i in range(800):
+    for j in range(4):
+        X_train.append(X_aux[j])
+        Y_train.append(Y_aux[j])
+
+# Setemos el numero de vecinos a considerar
+Knn = KNeighborsClassifier(n_neighbors = 2)
+
+Knn.fit(X_train,Y_train)
+
+"""
+Knn.predict([[1,1]])
+array([1])
+Knn.predict([[0,0]])
+array([1])
+Knn.predict([[1,0]])
+array([0])
+Knn.predict([[0,1]])
+array([0])
+
+"""
